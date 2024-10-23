4486568fe47f920b991bf1d416fa481f09958966
sarwar <sarwar.cse.diu@gmail.com>
2024-10-23 16:25:54 +0600

    search page

--- BlogSite/BlogSite.Web/wwwroot/js/site.js
+++ BlogSite/BlogSite.Web/wwwroot/js/site.js
@@ -1,4 +1,16 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
+    $("#btnTour").click(function () {
+        $(".search-form").hide(); // Hide all search forms
+        $("#tourSearch").show();  // Show the Tour search form
+    });
 
-// Write your JavaScript code.
+    $("#btnHotel").click(function () {
+        $(".search-form").hide();
+        $("#hotelSearch").show(); // Show the Hotel search form
+    });
+
+    $("#btnFlight").click(function () {
+        $(".search-form").hide();
+        $("#flightSearch").show(); // Show the Flight search form
+    });
+});
